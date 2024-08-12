c7bc87e07e04ac5771df7785a03871c5faa9cac3
Jung Hyuk Jin <richard960602@gmail.com>
2024-08-12 14:01:32 +0900

    index.html 'main' slide done

--- www/js/index.js
+++ www/js/index.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     let slideIndex = 0;
     const slides = document.querySelector('.slides');
-    const dots = document.querySelectorAll('.dots span');
-    const totalSlides = dots.length;
+    const totalSlides = document.querySelectorAll('.slides img').length; // 슬라이드 이미지의 개수
 
     function showSlides() {
         slideIndex = (slideIndex + 1) % totalSlides; // 슬라이드 순환
         updateSlidePosition();
-        updateDots();
-        setTimeout(showSlides, 3000); // 3초마다 슬라이드 전환
+        setTimeout(showSlides, 5000); // 5초마다 슬라이드 전환
     }
 
     function updateSlidePosition() {
@@ -16,28 +14,6 @@ document.addEventListener("DOMContentLoaded", function() {
         slides.style.transform = `translateX(${-slideWidth * slideIndex}%)`;
     }
 
-    function updateDots() {
-        dots.forEach((dot, index) => {
-            dot.classList.remove('active');
-            if (index === slideIndex) {
-                dot.classList.add('active');
-            }
-        });
-    }
-
-    function currentSlide(n) {
-        clearTimeout(autoSlideTimer); // 자동 전환 타이머 초기화
-        slideIndex = n;
-        updateSlidePosition();
-        updateDots();
-        autoSlideTimer = setTimeout(showSlides, 3000); // 다시 자동 전환 시작
-    }
-
-    dots.forEach((dot, index) => {
-        dot.addEventListener('click', function() {
-            currentSlide(index);
-        });
-    });
-
-    let autoSlideTimer = setTimeout(showSlides, 3000); // 초기 슬라이드 전환 시작
+    // 초기 슬라이드 전환 시작
+    setTimeout(showSlides, 5000);
 });
